b22e6dd4e128bfd10bdcfd91a9b78c7fc89985b9
joyet-simon <joyet.simon@hotmail.fr>
2018-11-07 10:55:07 +0100

    update init

--- Balles/Form1.Designer.cs
+++ Balles/Form1.Designer.cs
@@ -28,13 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pbBalle = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBalle)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbBalle
+            // 
+            this.pbBalle.Image = ((System.Drawing.Image)(resources.GetObject("pbBalle.Image")));
+            this.pbBalle.Location = new System.Drawing.Point(371, 106);
+            this.pbBalle.Name = "pbBalle";
+            this.pbBalle.Size = new System.Drawing.Size(100, 100);
+            this.pbBalle.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pbBalle.TabIndex = 0;
+            this.pbBalle.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pbBalle);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbBalle)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pbBalle;
     }
 }
 
--- Balles/Form1.cs
+++ Balles/Form1.cs
@@ -12,9 +12,26 @@ namespace Balles
 {
     public partial class Form1 : Form
     {
+        Timer tm = new Timer();
+        Int32 vitesseX = 5;
+        Int32 vitesseY = 5;
         public Form1()
         {
             InitializeComponent();
+
+            tm.Interval = 50;
+            tm.Tick += bouger; ;
+            tm.Enabled = true;
+        }
+
+        private void bouger(Object sender, EventArgs e)
+        {
+            if (pbBalle.Left < 0) { vitesseX = -vitesseX; }
+            if (pbBalle.Left > this.Width - pbBalle.Width - 15) { vitesseX = -vitesseX; }
+            if (pbBalle.Top < 0) { vitesseY = -vitesseY; }
+            if (pbBalle.Top > this.Height - pbBalle.Height - 38) { vitesseY = -vitesseY; }
+            pbBalle.Left = pbBalle.Left + vitesseX;
+            pbBalle.Top = pbBalle.Top + vitesseY;
         }
     }
 }
